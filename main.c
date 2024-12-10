9b4d017e30e960e17c2201f577b649abd9755dee
Lilian Varinot <varinot@eurecom.fr>
2024-12-10 10:38:42 +0100

    add

--- main.c
+++ main.c
@@ -82,7 +82,7 @@ int main( void )
     set_tacho_ramp_down_sp( r_sn, 2000 );
     set_tacho_command_inx( l_sn, TACHO_RUN_TIMED );
     set_tacho_command_inx( r_sn, TACHO_RUN_TIMED );
-
+    Sleep( 100 );
     set_tacho_speed_sp( l_sn, l_max_speed);
     set_tacho_speed_sp( r_sn, r_max_speed);
     set_tacho_time_sp( l_sn, 5000 );
@@ -93,7 +93,7 @@ int main( void )
     set_tacho_ramp_down_sp( r_sn, 1000 );
     set_tacho_command_inx( l_sn, TACHO_RUN_TIMED );
     set_tacho_command_inx( r_sn, TACHO_RUN_TIMED );
-
+    Sleep( 100 );
     set_tacho_speed_sp( l_sn, l_max_speed);
     set_tacho_speed_sp( r_sn, r_max_speed);
     set_tacho_time_sp( l_sn, 5000 );
@@ -103,6 +103,18 @@ int main( void )
     set_tacho_ramp_down_sp( l_sn, 500 );
     set_tacho_ramp_down_sp( r_sn, 500 );
     set_tacho_command_inx( l_sn, TACHO_RUN_TIMED );
+    set_tacho_command_inx( r_sn, TACHO_RUN_TIMED );
+    Sleep( 100 );
+    set_tacho_speed_sp( l_sn, l_max_speed);
+    set_tacho_time_sp( l_sn, 3000 );
+    set_tacho_ramp_up_sp( l_sn, 1000 );
+    set_tacho_ramp_down_sp( l_sn, 1000 );
+    set_tacho_command_inx( l_sn, TACHO_RUN_TIMED );
+    Sleep( 100 );
+    set_tacho_speed_sp( r_sn, r_max_speed);
+    set_tacho_time_sp( r_sn, 3000 );
+    set_tacho_ramp_up_sp( r_sn, 1000 );
+    set_tacho_ramp_down_sp( r_sn, 1000 );
     set_tacho_command_inx( r_sn, TACHO_RUN_TIMED );
   } else {
       printf( "LEGO_EV3_M_MOTOR 1 is NOT found\n" );
