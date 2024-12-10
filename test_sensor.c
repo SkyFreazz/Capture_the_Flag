f657d290c54b82baed3ee3365afb7cc83c380f9c
Lilian Varinot <varinot@eurecom.fr>
2024-12-10 15:38:35 +0100

    test

--- test_sensor.c
+++ test_sensor.c
@@ -35,14 +35,34 @@ static bool _check_pressed( uint8_t sn )
 
 void main ()
 {
-    uint8_t sn_touch;
-    uint8_t sn_color;
-    uint8_t sn_compass;
-    uint8_t sn_sonar;
-    int val;
-    float value;
+  uint8_t sn_touch;
+  uint8_t sn_color;
+  uint8_t sn_compass;
+  uint8_t sn_sonar;
+  int val;
+  float value;
 
-    ev3_sensor_init();
+  ev3_sensor_init();
+  printf( "Found sensors:\n" );
+  for ( i = 0; i < DESC_LIMIT; i++ ) {
+    if ( ev3_sensor[ i ].type_inx != SENSOR_TYPE__NONE_ ) {
+      printf( "  type = %s\n", ev3_sensor_type( ev3_sensor[ i ].type_inx ));
+      printf( "  port = %s\n", ev3_sensor_port_name( i, s ));
+      if ( get_sensor_mode( i, s, sizeof( s ))) {
+        printf( "  mode = %s\n", s );
+      }
+      if ( get_sensor_num_values( i, &n )) {
+        for ( ii = 0; ii < n; ii++ ) {
+          if ( get_sensor_value( ii, i, &val )) {
+            printf( "  value%d = %d\n", ii, val );
+          }
+        }
+      }
+    }
+  }
+  if ( ev3_search_sensor( LEGO_EV3_TOUCH, &sn_touch, 0 )) {
+    printf( "TOUCH sensor is found, press BUTTON for EXIT...\n" );
+  }
     for ( ; ; ){
     if ( ev3_search_sensor( LEGO_EV3_COLOR, &sn_color, 0 )) {
       printf( "COLOR sensor is found, reading COLOR...\n" );
