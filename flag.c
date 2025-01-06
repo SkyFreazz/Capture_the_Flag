3511a307d4e7ec6550049b461b3a46ddb9fade8a
Lilian Varinot <varinot@eurecom.fr>
2025-01-06 21:01:57 +0100

    code of the phase 2

---
+++ flag.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "ev3.h"
+#include "ev3_port.h"
+#include "ev3_tacho.h"
+#include "ev3_sensor.h"
+#include "mvt.h"
+
+srand(time(NULL));
+
+// WIN32 /////////////////////////////////////////
+#ifdef __WIN32__
+
+#include <windows.h>
+
+// UNIX //////////////////////////////////////////
+#else
+
+#include <unistd.h>
+#define Sleep( msec ) usleep(( msec ) * 1000 )
+
+//////////////////////////////////////////////////
+#endif
+
+const char const *color[] = { "?", "BLACK", "BLUE", "GREEN", "YELLOW", "RED", "WHITE", "BROWN" };
+#define COLOR_COUNT  (( int )( sizeof( color ) / sizeof( color[ 0 ])))
+
+static bool _check_pressed( uint8_t sn )
+{
+  int val;
+
+  if ( sn == SENSOR__NONE_ ) {
+    return ( ev3_read_keys(( uint8_t *) &val ) && ( val & EV3_KEY_UP ));
+  }
+  return ( get_sensor_value( 0, sn, &val ) && ( val != 0 ));
+}
+
+int main(int sn_sonar, int sn_touch, int sn_color, int sn_compass, uint8_t l_sn, uint8_t r_sn, FLAGS_T l_state, FLAGS_T r_state)
+{
+  int tol = 2; //set a tolerance for the angle
+  int angle;
+  int last_angle;
+
+  //Go back to initial angle (0°)
+  while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+    if ( !get_sensor_value0(sn_compass, &value )) {
+      angle = 0;
+    }
+
+    if (abs(angle % 360) <= tol){
+      last_angle = angle;
+      break;
+    }
+
+    angle = (angle % 360 + 360) % 360;
+
+    if (angle >= 180){
+      mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+    } else {
+      mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+    }
+  }
+
+  //choose the flag to grab
+  int flag = rand() % 2; //0: left, 1: right
+
+  if (flag == 0){
+  //Turn 90 left
+    while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+      if ( !get_sensor_value0(sn_compass, &value )) {
+        angle = 0;
+      }
+
+      angle += 90;
+
+      if (abs(angle % 360) <= tol){
+        last_angle = angle;
+        break;
+      }
+
+      angle = (angle % 360 + 360) % 360;
+
+      if (angle >= 180){
+        mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      } else {
+        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+      }
+  }
+  }
+
+  //Move forward to the wall
+  while (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+      if (!get_sensor_value0(sn_sonar, &value )) {
+        value = 0;
+      }
+      
+      if (value <= 30){
+        break;
+      }
+
+      mvt_forward(l_sn, r_sn, 100, 0, -2, -2, l_state, r_state);
+    }
+
+  //Turn 90 right
+  while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+    if ( !get_sensor_value0(sn_compass, &value )) {
+      angle = 0;
+    }
+
+    angle -= 90;
+
+    if (abs(angle % 360) <= tol){
+      last_angle = angle;
+      break;
+    }
+
+    angle = (angle % 360 + 360) % 360;
+
+    if (angle >= 180){
+      mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+    } else {
+      mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+    }
+  }
+
+  //Open the clamp
+  turn_arm(a_sn, 90, 0,  3,  a_state);
+
+  //Move to the flag
+  while (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+      if (!get_sensor_value0(sn_sonar, &value )) {
+        value = 0;
+      }
+      
+      if (value <= 15 && _check_pressed( sn_touch )){
+        break;
+      }
+
+      mvt_forward(l_sn, r_sn, 100, 0, -2, -2, l_state, r_state);
+    }
+
+  //Close the clamp
+  turn_arm(a_sn, -90, 0,  3,  a_state);
+
+  if (flag == 0){
+    //Turn 180 right
+    while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+      if ( !get_sensor_value0(sn_compass, &value )) {
+        angle = 0;
+      }
+
+      angle -= 180;
+
+      if (abs(angle % 360) <= tol){
+        last_angle = angle;
+        break;
+      }
+
+      angle = (angle % 360 + 360) % 360;
+
+      if (angle >= 180){
+        mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      } else {
+        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+      }
+    }
+  } else{
+    //Turn 90 right
+    while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+      if ( !get_sensor_value0(sn_compass, &value )) {
+        angle = 0;
+      }
+
+      angle -= 90;
+
+      if (abs(angle % 360) <= 2){
+        last_angle = angle;
+        break;
+      }
+
+      angle = (angle % 360 + 360) % 360;
+
+      if (angle >= 180){
+        mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      } else {
+        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+      }
+    }
+  }
+
+  //Move forward to the wall
+  while (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+    if (!get_sensor_value0(sn_sonar, &value )) {
+      value = 0;
+    }
+    
+    if (value <= 15 && _check_pressed( sn_touch )){
+      break;
+    }
+
+    mvt_forward(l_sn, r_sn, 100, 0, -2, -2, l_state, r_state);
+  }
+  
+  //Open the clamp
+  turn_arm(a_sn, 90, 0,  3,  a_state);
+
+  //Move backward a bit
+  mvt_forward(l_sn, r_sn, 500, 0, 2, 2, l_state, r_state);
+
+}
