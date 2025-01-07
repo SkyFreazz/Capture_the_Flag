fe73e97cb091c1e1a68d5e1619db6fac58ead965
Lilian Varinot <varinot@eurecom.fr>
2025-01-07 10:42:22 +0100

    test

--- main.c
+++ main.c
@@ -90,7 +90,7 @@ if (ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
   if ( !get_sensor_value0(sn_compass, &initial_angle )) {
     initial_angle = 0;
   }
-  printf("initial_angle: %i", initial_angle);
+  printf("initial_angle: %d", initial_angle);
 }
 
 mvt_forward(l_sn, r_sn, 1000, 0, 2, 2, l_state, r_state);
