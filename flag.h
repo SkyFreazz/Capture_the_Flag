ebb08d9db144595053fcb0b773a4ecdf771755d7
Lilian Varinot <varinot@eurecom.fr>
2025-01-07 08:47:30 +0100

    add .h file

---
+++ flag.h
@@ -0,0 +1,4 @@
+#ifndef FLAG_H
+#define FLAG_H
+
+catch_flag(int sn_sonar, int sn_touch, int sn_color, int sn_compass, uint8_t l_sn, uint8_t r_sn, FLAGS_T l_state, FLAGS_T r_state, int initial_angle);
---
+++ mvt.h
@@ -0,0 +1,9 @@
+#ifndef MVT_H
+#define MVT_H
+
+
+void turn_angle(uint8_t sn, int angle, int ramp, int vit, FLAGS_T state, int motor);
+
+void mvt_forward(uint8_t l_sn, uint8_t r_sn, int time, int ramp, int l_vit, int r_vit, FLAGS_T l_state, FLAGS_T r_state);
+
+void mvt_turn_one_wheel(uint8_t sn, int time, int ramp, int vit, FLAGS_T state, int motor);
