4e44505e6947dd5db64a042fb86fa17df565c38f
Lilian Varinot <varinot@eurecom.fr>
2025-01-07 09:48:41 +0100

    bug fixed

--- flag.c
+++ flag.c
@@ -38,24 +38,26 @@ int catch_flag(int sn_sonar, int sn_touch, int sn_compass, uint8_t l_sn, uint8_t
   int last_angle;
 
   //Go back to initial angle (0°)
-  while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
-    if ( !get_sensor_value0(sn_compass, &angle )) {
-      angle = 0;
-    }
+  if(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+    while(true){
+      if ( !get_sensor_value0(sn_compass, &angle )) {
+        angle = 0;
+      }
 
-    angle -= initial_angle;
+      angle -= initial_angle;
 
-    if (abs(angle % 360) <= tol){
-      last_angle = angle;
-      break;
-    }
+      if (abs(angle % 360) <= tol){
+        last_angle = angle;
+        break;
+      }
 
-    angle = (angle % 360 + 360) % 360;
+      angle = (angle % 360 + 360) % 360;
 
-    if (angle >= 180){
-      mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
-    } else {
-      mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      if (angle >= 180){
+        mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      } else {
+        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+      }
     }
   }
 
@@ -64,31 +66,34 @@ int catch_flag(int sn_sonar, int sn_touch, int sn_compass, uint8_t l_sn, uint8_t
 
   if (flag == 0){
   //Turn 90 left
-    while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
-      if ( !get_sensor_value0(sn_compass, &angle )) {
-        angle = 0;
+    if(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+      while(true){
+        if ( !get_sensor_value0(sn_compass, &angle )) {
+          angle = 0;
+        }
+
+        angle -= initial_angle;
+        angle += 90;
+
+        if (abs(angle % 360) <= tol){
+          last_angle = angle;
+          break;
+        }
+
+        angle = (angle % 360 + 360) % 360;
+
+        if (angle >= 180){
+          mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+        } else {
+          mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+        }
       }
-
-      angle -= initial_angle;
-      angle += 90;
-
-      if (abs(angle % 360) <= tol){
-        last_angle = angle;
-        break;
-      }
-
-      angle = (angle % 360 + 360) % 360;
-
-      if (angle >= 180){
-        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
-      } else {
-        mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
-      }
-  }
+    }
   }
 
   //Move forward to the wall
-  while (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+  if (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+    while(true){
       if (!get_sensor_value0(sn_sonar, &value )) {
         value = 0;
       }
@@ -97,78 +102,13 @@ int catch_flag(int sn_sonar, int sn_touch, int sn_compass, uint8_t l_sn, uint8_t
         break;
       }
 
-      mvt_forward(l_sn, r_sn, 100, 0, -2, -2, l_state, r_state);
-    }
-
-  //Turn 90 right
-  while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
-    if ( !get_sensor_value0(sn_compass, &angle )) {
-      angle = 0;
-    }
-
-    angle -= initial_angle;
-    angle -= 90;
-
-    if (abs(angle % 360) <= tol){
-      last_angle = angle;
-      break;
-    }
-
-    angle = (angle % 360 + 360) % 360;
-
-    if (angle >= 180){
-      mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
-    } else {
-      mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      mvt_forward(l_sn, r_sn, 100, 0, 2, 2, l_state, r_state);
     }
   }
 
-  //Open the clamp
-  turn_angle(a_sn, 90, 0, 3, a_state, 67);
-
-  //Move to the flag
-  while (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
-      if (!get_sensor_value0(sn_sonar, &value )) {
-        value = 0;
-      }
-      
-      if (value <= 10 && _check_pressed( sn_touch )){
-        break;
-      }
-
-      mvt_forward(l_sn, r_sn, 100, 0, -2, -2, l_state, r_state);
-    }
-
-  //Close the clamp
-  turn_angle(a_sn, -90, 0, 3, a_state, 67);
-
-  if (flag == 0){
-    //Turn 180 right
-    while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
-      if ( !get_sensor_value0(sn_compass, &angle )) {
-        angle = 0;
-      }
-
-      angle -= initial_angle;
-      angle -= 180;
-
-      if (abs(angle % 360) <= tol){
-        last_angle = angle;
-        break;
-      }
-
-      angle -= initial_angle;
-      angle = (angle % 360 + 360) % 360;
-
-      if (angle >= 180){
-        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
-      } else {
-        mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
-      }
-    }
-  } else{
-    //Turn 90 right
-    while(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+  //Turn 90 right
+  if(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+    while(true){
       if ( !get_sensor_value0(sn_compass, &angle )) {
         angle = 0;
       }
@@ -176,7 +116,7 @@ int catch_flag(int sn_sonar, int sn_touch, int sn_compass, uint8_t l_sn, uint8_t
       angle -= initial_angle;
       angle -= 90;
 
-      if (abs(angle % 360) <= 2){
+      if (abs(angle % 360) <= tol){
         last_angle = angle;
         break;
       }
@@ -184,31 +124,107 @@ int catch_flag(int sn_sonar, int sn_touch, int sn_compass, uint8_t l_sn, uint8_t
       angle = (angle % 360 + 360) % 360;
 
       if (angle >= 180){
-        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
-      } else {
         mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+      } else {
+        mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+      }
+    }
+  }
+
+  //Open the clamp
+  turn_angle(a_sn, 90, 0, 3, a_state, 67);
+
+  //Move to the flag
+  if(ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+    while(true){
+      if (!get_sensor_value0(sn_sonar, &value )) {
+        value = 0;
+      }
+      
+      if (value <= 10 && _check_pressed( sn_touch )){
+        break;
+      }
+
+      mvt_forward(l_sn, r_sn, 100, 0, 2, 2, l_state, r_state);
+    }
+  }
+
+  //Close the clamp
+  turn_angle(a_sn, -90, 0, 3, a_state, 67);
+
+  if (flag == 0){
+    //Turn 180 right
+    if(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+      while(true){
+        if ( !get_sensor_value0(sn_compass, &angle )) {
+          angle = 0;
+        }
+
+        angle -= initial_angle;
+        angle -= 180;
+
+        if (abs(angle % 360) <= tol){
+          last_angle = angle;
+          break;
+        }
+
+        angle -= initial_angle;
+        angle = (angle % 360 + 360) % 360;
+
+        if (angle >= 180){
+          mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+        } else {
+          mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+        }
+      }
+    }
+  } else {
+    //Turn 90 right
+    if(ev3_search_sensor(HT_NXT_COMPASS, &sn_compass,0)){
+      while(true){
+        if ( !get_sensor_value0(sn_compass, &angle )) {
+          angle = 0;
+        }
+
+        angle -= initial_angle;
+        angle -= 90;
+
+        if (abs(angle % 360) <= 2){
+          last_angle = angle;
+          break;
+        }
+
+        angle = (angle % 360 + 360) % 360;
+
+        if (angle >= 180){
+          mvt_forward(l_sn, r_sn, 100, 0, 2, -2, l_state, r_state);
+        } else {
+          mvt_forward(l_sn, r_sn, 100, 0, -2, 2, l_state, r_state);
+        }
       }
     }
   }
 
   //Move forward to the wall
-  while (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
-    if (!get_sensor_value0(sn_sonar, &value )) {
-      value = 0;
-    }
-    
-    if (value <= 10 && _check_pressed( sn_touch )){
-      break;
-    }
+  if (ev3_search_sensor(LEGO_EV3_US, &sn_sonar,0)){
+    while(true){
+      if (!get_sensor_value0(sn_sonar, &value )) {
+        value = 0;
+      }
+      
+      if (value <= 10 && _check_pressed( sn_touch )){
+        break;
+      }
 
-    mvt_forward(l_sn, r_sn, 100, 0, -2, -2, l_state, r_state);
+      mvt_forward(l_sn, r_sn, 100, 0, 2, 2, l_state, r_state);
+    }
   }
   
   //Open the clamp
   turn_angle(a_sn, 90, 0, 3, a_state, 67);
 
   //Move backward a bit
-  mvt_forward(l_sn, r_sn, 500, 0, 2, 2, l_state, r_state);
+  mvt_forward(l_sn, r_sn, 500, 0, -2, -2, l_state, r_state);
 
 return 0;
 }
