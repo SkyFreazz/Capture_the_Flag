4e53a489cdb1cf3683ca465de62aca9142967df4
mosqueda <leonardo-zadkiel.mosqueda-hernandez@eurecom.fr>
2025-01-09 10:44:38 +0000

    Replace step1.h

--- step1.h
+++ step1.h
@@ -3,9 +3,9 @@
 
 void mvt_motor(uint8_t l_sn, uint8_t r_sn, int time, int ramp, int l_vit, int r_vit, FLAGS_T l_state, FLAGS_T r_state);
 
-float sonar(uint8_t sn_sonar, float value);
-
 float compas(uint8_t sn_compass, float value);
+
+float sonar(uint8_t sn_sonar, float value);
 
 int couleur(uint8_t sn_color, int val, int stp);
 
