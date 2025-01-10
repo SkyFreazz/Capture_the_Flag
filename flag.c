70fcda3c129a6f6cdde64d555e26641f13683893
Lilian Varinot <varinot@eurecom.fr>
2025-01-10 11:25:42 +0100

    test

--- flag.c
+++ flag.c
@@ -48,7 +48,7 @@ int catch_flag(float initial_angle)
 
   //Move forward to the wall
   forward_to_wall(150, last_angle, 1, 2);
-  mvt_forward(100, 0, -2, -2);
+  //mvt_forward(100, 0, -2, -2);
 
   //Turn 90 right
   printf("angle to reach: %f", last_angle + 90.0);
@@ -65,7 +65,7 @@ int catch_flag(float initial_angle)
 
   //Move to the flag
   forward_to_wall(150, last_angle, 1, 1);
-  mvt_forward(100, 0, -2, -2);
+  //mvt_forward(100, 0, -2, -2);
 
 
   //Close the clamp
