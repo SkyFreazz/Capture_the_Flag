274c4bad02584ee26e1e0e593a08710034f0c786
Lilian Varinot <varinot@eurecom.fr>
2025-01-21 11:20:01 +0100

    bug opti

--- code/functions/mvt.c
+++ code/functions/mvt.c
@@ -235,7 +235,7 @@ void forward_to_base(float dist_min, float initial_angle, float first_angle, flo
         printf("too long, phase 2");
         phase = 2;
       }
-      if (diff_t >= 15.0 and phase == 2){
+      if (diff_t >= 15.0 && phase == 2){
         phase = 3;
       }
       
